c2f2e7caa69fc1b039aa118ace1413a93ff5dfd4
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-10-18 16:19:02 +0300

    feat(Tab): add menu, actions, contacts, cart

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,54 +1,34 @@
 <template>
   <div class="city-panel">
     <header class="header">
-      <div class="header-content">
-        <div class="city-time">
-          <div class="container">
-            <img class="gps" src="../../assets/img/gps.svg" />
-            <select v-model="cityName" @change="changeCity" class="city-select">
-              <option v-for="city in cities" :key="city">{{ city }}</option>
-            </select>
-          </div>
-          <div class="container">
-            <img class="time" src="../../assets/img/time.svg" />
-            с {{ openingTime }} до {{ closingTime }}
-          </div>
-          <div class="container">
-            <img class="phone" src="../../assets/img/phone.svg" />
-            {{ number }}
-          </div>
+      <div class="header-content ">
+        <div class="container">
+          <img class="gps" src="../../assets/img/gps.svg" />
+          <select v-model="cityName" @change="changeCity" class="city-select">
+            <option v-for="city in cities" :key="city">{{ city }}</option>
+          </select>
         </div>
+        <div class="container">
+          <img class="time" src="../../assets/img/time.svg" />
+          с {{ openingTime }} до {{ closingTime }}
+        </div>
+        <div class="container">
+          <img class="phone" src="../../assets/img/phone.svg" />
+          {{ number }}
+        </div>
+      </div>
+      <div class="menu-actions">
+        <span class="menu style-head">Меню</span>
+        <span class="actions style-head">Акции</span>
+        <span class="contacts style-head">Контакты</span>
+        <span class="cart style-head">
+          <img src="../../assets/img/cart.svg" class="cart-icon" />
+          Корзина
+        </span>
       </div>
     </header>
   </div>
 </template>
-
-<script>
-import { ref } from 'vue';
-
-export default {
-  setup() {
-    const cityName = ref('Ярославль');
-    const openingTime = ref('9:00');
-    const closingTime = ref('23:00');
-    const number = ref('8 (800) 555-35-35');
-    const cities = ['Ярославль', 'Москва', 'Санкт-Петербург', 'Казань'];
-
-    const changeCity = (event) => {
-      cityName.value = event.target.value;
-    };
-
-    return {
-      cityName,
-      openingTime,
-      closingTime,
-      number,
-      cities,
-      changeCity,
-    };
-  },
-};
-</script>
 
 <style scoped>
 .city-panel {
@@ -56,20 +36,74 @@ export default {
   top: 0;
   left: 0;
   right: 0;
-  background-color: #f0f0f0;
   padding: 10px;
+  background-color: #f0f0f0;
+
 }
 
 .header-content {
-  margin-left: 200px;
   font-family: Arial, sans-serif;
   font-size: 18px;
-  align-items: center;
-}
-
-.city-time {
   display: flex;
   align-items: center;
+  margin-left: 200px;
+
+}
+
+.style-head {
+  display: inline-block;
+  margin: 10px;
+  border: 2px solid #333;
+  border-radius: 30px;
+  padding: 10px;
+  text-align: center;
+  width: 250px;
+  position: relative;
+  line-height: 1;
+
+
+}
+
+.cart {
+  padding-left: 450px;
+  border: none;
+  display: flex;
+  align-items: center;
+  margin-left: 20px; /* Adjust the margin for cart text alignment */
+  font-size: 25px; /* Увеличьте размер шрифта для меню */
+  font-weight: bold; /* Установите жирное начертание для меню */
+}
+
+.cart-icon {
+  height: 30px;
+  width:30px;
+  margin-right: 5px;
+}
+
+.menu-actions {
+  display: flex; /* Create a flex container for the menu, actions, and contacts */
+  align-items: center;
+
+}
+.menu {
+  margin-left: 200px;
+  margin-right: 20px;
+  font-size: 16px; /* Увеличьте размер шрифта для меню */
+  font-weight: bold; /* Установите жирное начертание для меню */
+}
+
+.actions {
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 16px; /* Увеличьте размер шрифта для действий */
+  font-weight: bold; /* Установите жирное начертание для действий */
+}
+
+.contacts {
+  margin-left: 20px;
+  margin-right: 0;
+  font-size: 16px; /* Увеличьте размер шрифта для контактов */
+  font-weight: bold; /* Установите жирное начертание для контактов */
 }
 
 .container {
@@ -108,3 +142,32 @@ export default {
   color: #333;
 }
 </style>
+
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const cityName = ref('Ярославль')
+    const openingTime = ref('9:00')
+    const closingTime = ref('23:00')
+    const number = ref('8 (800) 555-35-35')
+    const cities = ['Ярославль', 'Москва', 'Санкт-Петербург', 'Казань']
+
+    const changeCity = (event) => {
+      cityName.value = event.target.value
+    }
+
+    return {
+      cityName,
+      openingTime,
+      closingTime,
+      number,
+      cities,
+      changeCity
+    }
+  }
+}
+</script>
+
