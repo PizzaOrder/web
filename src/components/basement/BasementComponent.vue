1ab3b97a70c12334bc728ecca403e8df26dcb7fd
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-10-25 17:49:40 +0300

    fix: unpin basmant

--- src/components/basement/BasementComponent.vue
+++ src/components/basement/BasementComponent.vue
@@ -12,25 +12,24 @@ body {
 }
 
 .basement {
-  position: fixed;
-  bottom: 0;
   text-align: center;
   background-color: #333;
   color: white;
-  padding: 15px 20px; /* Добавим отступы вместо фиксированной высоты */
+  padding: 15px 0px;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 1rem; /* Используем относительные единицы измерения для размера шрифта */
+  font-size: 1rem;
+  position: relative; /* Возвращаем позицию к значению по умолчанию */
+  top: 100%; /* Поднимаем элемент над всеми компонентами */
 }
 
 /* Медиа-запрос для меньших экранов */
 @media (max-width: 600px) {
   .basement {
-    font-size: 0.9rem; /* Уменьшим размер шрифта для мобильных устройств */
-    padding: 10px 15px; /* Уменьшим отступы для мобильных устройств */
+    font-size: 0.9rem;
+    padding: 10px 15px;
   }
 }
-
 </style>
