e574c03dbd4b6a20340093e514a03b6130114500
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-10-28 14:10:17 +0300

    feat(discount): rework and change position

---
+++ src/components/body/DiscountMenuComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class='Discount'>Акции</div>
+
+
+</template>
+
+<style scoped>
+.Discount{
+  font-size: 40px;
+  margin-bottom: 40px;
+  font-weight: bold;
+}
+</style>
--- src/views/DiscountsComponent.vue
+++ src/views/DiscountsComponent.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
     <HeaderComponent />
+  </div>
+  <div class='Discount'>
+    <DiscountMenuComponent />
+  </div>
+  <div class='Basement'>
     <BasementComponent />
   </div>
+
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent } from 'vue'
+import DiscountMenuComponent from '@/components/body/DiscountMenuComponent.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import BasementComponent from '@/components/basement/BasementComponent.vue'
 
 export default defineComponent({
   name: 'DiscountComponent',
   components: {
+    DiscountMenuComponent,
     HeaderComponent,
     BasementComponent
   },
@@ -21,3 +29,22 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+
+@media (max-width: 768px) {
+  .Discount {
+    margin-top: 100px;
+    margin-left: 15px;
+  }
+}
+
+@media (min-width: 769px) {
+  .Discount {
+    margin-top: 150px;
+    margin-left: 30px;
+  }
+}
+
+
+</style>
