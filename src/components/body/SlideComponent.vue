e813f64602cf241e339b8f64df05ce2693933af8
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-10-28 16:05:45 +0300

    feat(discount): add new link for sliders

--- src/components/body/SlideComponent.vue
+++ src/components/body/SlideComponent.vue
@@ -9,7 +9,7 @@
       >
         <div v-for="(slide, index) in slides" :key="index" class="carousel-item">
           <router-link to='/discount' class="carousel-item">
-          <img :src="slide.image" :alt="slide.alt" ondragstart="return false" />
+          <img :src="slide.image" :alt="slide.alt" draggable="false"/>
           </router-link>
         </div>
       </div>
@@ -125,6 +125,7 @@ const prevSlide = () => {
   display: flex;
   justify-content: center;
   align-items: center;
+
 }
 
 .carousel-item img {
