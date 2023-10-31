fbbe40e064349d0aeefcbb0589ee789fdb56aef5
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-10-31 17:45:19 +0300

    feat(cart): add new page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeComponent from '@/views/HomeComponent.vue'
 import DiscountsComponent from '@/views/DiscountsComponent.vue'
-
+import CartAllComponent from '@/views/CartAllComponent.vue'
 const routes = [
   { path: '/', component: HomeComponent },
-  { path: '/discount', component: DiscountsComponent }
+  { path: '/discount', component: DiscountsComponent },
+  {path: '/cart',component: CartAllComponent}
 ]
 
 const router = createRouter({
---
+++ src/views/CartAllComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+import CartComponent from '@/components/body/CartComponent.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
+
+<template>
+  <div><HeaderComponent/></div>
+  <div class='Cart'><CartComponent/></div>
+
+</template>
+
+<style scoped>
+.Cart{
+  margin-top: 110px;
+}
+</style>
