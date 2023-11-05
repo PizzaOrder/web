d4716b961bcae3e7f207b85c12b4ca4a27bdcb5c
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-11-06 00:08:46 +0300

    feat(cart): promo for sum

--- src/components/body/CartComponent.vue
+++ src/components/body/CartComponent.vue
@@ -6,11 +6,11 @@
     </div>
     <input v-model='promoCode' placeholder='ввод промокода' />
     <button @click='checkPromoCode'>Проверить промокод</button>
-    <p v-if='promoCodeValid !== null'>
-      <span v-if='promoCodeValid'>Промокод действителен. Скидка применена.</span>
-      <span v-else>Промокод недействителен. Попробуйте другой промокод.</span>
+    <p v-if='discount'>
+      Промокод действителен. Скидка {{ discount }}% применена.
     </p>
-    <p>Общая сумма: ${{ totalPrice }}</p> <!-- Вывод общей суммы -->
+    <p v-else-if='promoCodeValid === false'>Промокод недействителен. Попробуйте другой промокод.</p>
+    <p>Общая сумма: ${{ totalPrice }}</p>
   </div>
 </template>
 
@@ -23,51 +23,51 @@ export default defineComponent({
   name: 'OrdersComponent',
   props: {
     promes: {
-      type: Array as PropType<{ id: number; code: string }[]>,
+      type: Array as PropType<{ id: number; code: string; discountOnPresent: number }[]>,
       required: true
     }
   },
   setup(props) {
     const promoCode = ref('')
     const promoCodeValid = ref<boolean | null>(null)
+    const discount = ref<number | null>(null)
 
     const orders = computed(() => globalState.orders)
 
     const totalPrice = computed(() => {
-      // Вычисление общей суммы заказов с учетом количества
-      const sum = orders.value.reduce((total, order) => {
-        // Проверяем, определено ли количество, если нет, то считаем его как 1
+      let sum = orders.value.reduce((total, order) => {
         const quantity = order.quantity ?? 1;
         return total + order.price * quantity;
       }, 0)
-      // Применение скидки, если промокод действителен
-      return promoCodeValid.value ? sum * 0.9 : sum // предполагаем скидку в 10%
+      if (discount.value) {
+        sum -= sum * (discount.value / 100);
+      }
+      return sum;
     })
 
     const checkPromoCode = () => {
+      promoCodeValid.value = false
+      discount.value = null
+
       if (promoCode.value === '') {
-        promoCodeValid.value = null // Сбросить состояние, если поле пустое
         return
       }
 
-      let flag = false
-      for (const prom of props.promes) {
-        if (promoCode.value === prom.code) {
-          flag = true
-          break
-        }
+      const foundPromo = props.promes.find(prom => promoCode.value === prom.code);
+      if (foundPromo) {
+        promoCodeValid.value = true
+        discount.value = foundPromo.discountOnPresent
       }
-      promoCodeValid.value = flag
     }
 
     return {
       promoCode,
       promoCodeValid,
+      discount,
       checkPromoCode,
-      totalPrice, // Теперь totalPrice доступен для использования в шаблоне
+      totalPrice,
       orders
     }
   }
 })
 </script>
-
