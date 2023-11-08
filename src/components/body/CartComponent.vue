252c77612a0f332cf978529c5042a6305ba4406c
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-11-08 16:35:58 +0300

    feat(cart): style

--- src/components/body/CartComponent.vue
+++ src/components/body/CartComponent.vue
@@ -7,8 +7,8 @@
     </div>
     <div>Доставка в город {{ city }}</div>
     <div>
-      <span><button>Забрать самому</button></span>
-      <span><button @click='toggleHome'>Доставка на дом</button></span>
+      <button class="style-head text-center">Забрать самому</button>
+      <button class="style-head" @click="toggleHome">Доставка на дом</button>
       <div v-if='showHome'>
         <p><input type='text' id='street' placeholder='Например: улица Бебр, 1'></p>
         <p>
@@ -24,11 +24,11 @@
     </div>
     <input v-model='promoCode' placeholder='ввод промокода' />
     <button @click='checkPromoCode'>Проверить промокод</button>
-    <p v-if='discount'>
+    <p class="text-center" v-if="discount">
       Промокод действителен. Скидка {{ discount }}% применена.
     </p>
-    <p v-else-if='promoCodeValid === false'>Промокод недействителен. Попробуйте другой промокод.</p>
-    <p>Общая сумма: ${{ totalPrice }}</p>
+    <p class="text-center" v-else-if="promoCodeValid === false">Промокод недействителен. Попробуйте другой промокод.</p>
+    <p class="text-center">Общая сумма: ${{ totalPrice }}</p>
     <div><button>Оплатить</button></div>
   </div>
 </template>
@@ -107,3 +107,76 @@ export default defineComponent({
   }
 })
 </script>
+<style scoped>
+button,
+.style-head {
+  display: inline-block;
+  margin: 10px;
+  padding: 8px 16px;
+  border: none;
+  background-color: #ff5733;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.3s;
+  border-radius: 5px;
+  font-weight: bold;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+div {
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+div[v-for] {
+  background-color: #f9f9f9;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+div[v-for] img {
+  width: 50px;
+  margin-right: 10px;
+}
+
+input[type='text'],
+input[type='number'] {
+  padding: 5px;
+  margin: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  text-align: center;
+}
+
+input[type='text']::placeholder,
+input[type='number']::placeholder {
+  color: #aaa;
+}
+
+input[type='text']:focus,
+input[type='number']:focus {
+  border-color: #90cdf4;
+  outline: none;
+}
+
+p {
+  color: #333;
+  margin: 5px 0;
+}
+
+p {
+  font-weight: bold;
+  text-align: center;
+}
+
+.text-center {
+  text-align: center;
+}
+
+
+</style>
