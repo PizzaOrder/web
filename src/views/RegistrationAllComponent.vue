3d10865dd5117faf0a96cd4c793023d903a221fa
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-11-25 19:05:03 +0300

    feat(reg): add field login

--- src/views/RegistrationAllComponent.vue
+++ src/views/RegistrationAllComponent.vue
@@ -1,9 +1,13 @@
 <script lang="ts">
 import RegComponent from '@/components/registration/RegComponent.vue'
+import HeaderComponent from '@/components/header/HeaderComponent.vue'
 import { defineComponent } from 'vue'
+import BasementComponent from '@/components/basement/BasementComponent.vue'
 export default defineComponent({
   components: {
-    RegComponent
+    HeaderComponent,
+    RegComponent,
+    BasementComponent
   },
   setup() {
     return {}
@@ -12,7 +16,9 @@ export default defineComponent({
 </script>
 
 <template>
+  <div><HeaderComponent/></div>
   <div><RegComponent/></div>
+  <div><BasementComponent/></div>
 
 </template>
 
