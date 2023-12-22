b08100e401ef58df76768ebeaf45d5614457dbc0
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2023-12-22 03:48:46 +0300

    feat: admin

--- src/components/admin/AdminComponent.vue
+++ src/components/admin/AdminComponent.vue
@@ -1,39 +1,38 @@
 <template>
   <div>
-    <h2>All Orders</h2>
-    <div v-if="loading">Loading orders...</div>
+    <router-link to='/'>
+      <button>вернуться в меню</button>
+    </router-link>
+    <h2 class="text-2xl font-bold mb-4">All Orders</h2>
+    <div v-if="loading" class="text-gray-500">Loading orders...</div>
     <div v-else>
-      <div v-for="(orders, userId) in ordersByUser" :key="userId">
-        <h3>User ID: {{ userId }}</h3>
-        <ul>
-          <li v-for="(order, index) in orders" :key="index">
-            <div :class="statusClass(order.newStatus)">
-              <p :class="statusClass(order.newStatus)">Order ID: {{ order.id }}</p>
-              <p :class="statusClass(order.newStatus)">User ID: {{ order.user_id }}</p>
-              <p :class="statusClass(order.newStatus)">Promo ID: {{ order.promo_id }}</p>
-              <p :class="statusClass(order.newStatus)">Total Cost: {{ order.total_cost }}</p>
-              <p :class="statusClass(order.newStatus)">Order Date: {{ order.order_date }}</p>
-              <p :class="statusClass(order.newStatus)">Order Status: {{ order.order_status }}</p>
-              <p :class="statusClass(order.newStatus)">Order Items: {{ JSON.stringify(order.order_items, null, 2) }}</p>
-            </div>
-            <div>
-              <label :for="'status-select-' + order.id">Change Status:</label>
-              <select :id="'status-select-' + order.id" v-model="order.newStatus" @change="saveStatusToLocal(order.id, order.newStatus)">
-                <option value="Принят">Принят</option>
-                <option value="Готовится">Готовится</option>
-                <option value="Готов">Готов</option>
-                <option value="Выдан">Выдан</option>
-              </select>
-              <button @click="updateStatus(order.id, order.newStatus)">Update Status</button>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li v-for="(order, index) in ordersByDate" :key="index">
+          <div>
+            <p>Order ID: {{ order.id }}</p>
+            <p>User ID: {{ order.user_id }}</p>
+            <p>Promo ID: {{ order.promo_id }}</p>
+            <p>Total Cost: {{ order.total_cost }}</p>
+            <p>Order Date: {{ formatDate(order.order_date) }}</p>
+            <p :class="'order-status-' + order.order_status">Order Status: {{ order.order_status }}</p>
+            <p>Order Items: {{ JSON.stringify(order.order_items, null, 2) }}</p>
+          </div>
+          <div>
+            <label :for="'status-select-' + order.id">Change Status:</label>
+            <select :id="'status-select-' + order.id" v-model="order.newStatus" @change="saveStatusToLocal(order.id, order.newStatus)">
+              <option value="Принят">Принят</option>
+              <option value="Готовится">Готовится</option>
+              <option value="Готов">Готов</option>
+              <option value="Выдан">Выдан</option>
+            </select>
+            <button @click="updateStatus(order.id, order.newStatus)">Update Status</button>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
-<!-- Оставьте остальной код без изменений -->
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
 import { useAdminStore } from '@/Pinia/orderAllStore';
@@ -49,13 +48,19 @@ export default defineComponent({
     };
 
     const adminStore = useAdminStore();
-    const ordersByUser = ref({});
+    const ordersByDate = ref([]);
     const loading = ref(true);
 
     const fetchOrders = async () => {
       try {
         const data = await adminStore.fetchAllOrders();
-        ordersByUser.value = groupOrdersByUser(data);
+
+        const sortedOrders = data.sort((a, b) => new Date(b.order_date) - new Date(a.order_date));
+
+        ordersByDate.value = sortedOrders.map(order => ({
+          ...order,
+          newStatus: loadStatusFromLocal(order.id) || order.status
+        }));
       } catch (error) {
         console.error('Failed to fetch orders:', error);
       } finally {
@@ -63,68 +68,50 @@ export default defineComponent({
       }
     };
 
-    const groupOrdersByUser = (orders) => {
-      return orders.reduce((acc, order) => {
-        if (!acc[order.user_id]) {
-          acc[order.user_id] = [];
-        }
-        // Load newStatus from local storage or use order.status
-        const newStatus = loadStatusFromLocal(order.id) || order.status;
-        acc[order.user_id].push({ ...order, newStatus });
-        return acc;
-      }, {});
-    };
-
     const updateStatus = async (orderId, newStatus) => {
       try {
         await adminStore.updateOrderStatus(orderId, newStatus);
-        saveStatusToLocal(orderId, newStatus); // Save newStatus to local storage
-        await fetchOrders(); // Refetch orders to update the list
+        saveStatusToLocal(orderId, newStatus);
+        await fetchOrders();
       } catch (error) {
         console.error('Failed to update order status:', error);
       }
     };
 
-    const statusClass = (newStatus) => {
-      switch (newStatus) {
-        case 'Принят':
-          return 'status-accepted';
-        case 'Готовится':
-          return 'status-preparing';
-        case 'Готов':
-          return 'status-ready';
-        case 'Выдан':
-          return 'status-delivered';
-        default:
-          return ''; // По умолчанию не добавляем класс
-      }
+    const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      return date.toLocaleDateString('ru-RU', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+      });
     };
 
     onMounted(() => {
       fetchOrders();
     });
 
-    return { ordersByUser, loading, updateStatus, saveStatusToLocal, statusClass };
+    return { ordersByDate, loading, updateStatus, saveStatusToLocal, formatDate };
   },
 });
 </script>
 
+<style scoped>
+.order-status-Принят {
+  color: red;
+}
 
-
-<style>
-.status-accepted {
+.order-status-Готовится {
   color: green;
 }
 
-.status-preparing {
-  color: orange;
-}
-
-.status-ready {
+.order-status-Готов {
   color: blue;
 }
 
-.status-delivered {
-  color: purple;
+.order-status-Выдан {
+  color: orange;
 }
 </style>
